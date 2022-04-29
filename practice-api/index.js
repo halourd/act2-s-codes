f7b238f6d5f84cfeae0c562767131a250dfa95b5
Harold Gallego <haroldgallego@student.laverdad.edu.ph>
2022-04-29 19:02:53 +0800

    Add practice-api

---
+++ practice-api/index.js
@@ -0,0 +1,11 @@
+const express = require(express);
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = 4000;
+app.listen(PORT, () => console.log('Listening on port 4000....'));
+
+
